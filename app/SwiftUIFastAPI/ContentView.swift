043876d9984e566d1c52e903a7f3ac5b1cdd396b
sattsat <syuuatit141offer@gmail.com>
2025-02-04 21:55:00 +0900

    init commit

---
+++ app/SwiftUIFastAPI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIFastAPI
+//  
+//  Created by SATTSAT on 2025/02/04
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/SwiftUIFastAPI/SwiftUIFastAPIApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIFastAPIApp.swift
+//  SwiftUIFastAPI
+//  
+//  Created by SATTSAT on 2025/02/04
+//  
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFastAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
